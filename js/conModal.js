be9e73ae068034c86e74e7f0d115043ec9e053fd
Seolyu ♪ *҉＼( ˆᴗˆ )🌸✩ <seolyucode@gmail.com>
2020-04-19 03:57:09 +0900

    pjt01 | 2페이지 추가

--- js/conModal.js
+++ js/conModal.js
@@ -55,3 +55,33 @@ function showSlides02(n) {
     }
     slides[slideIndex02 - 1].style.display = "block";
 }
+
+
+// 코딩
+var slideIndex04 = 1;
+showSlides04(slideIndex04);
+
+// Next/previous controls
+function plusSlides04(n) {
+    showSlides04(slideIndex04 += n);
+}
+
+// Thumbnail image controls
+function currentSlide04(n) {
+    showSlides04(slideIndex04 = n);
+}
+
+function showSlides04(n) {
+    var i;
+    var slides = document.getElementsByClassName("ACAC04");
+    if (n > slides.length) {
+        slideIndex04 = 1
+    }
+    if (n < 1) {
+        slideIndex04 = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex04 - 1].style.display = "block";
+}
